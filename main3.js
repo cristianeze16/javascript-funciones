bd711f5e38cbf4368dccb368f4b924ad9000a6c3
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-10-13 08:45:25 -0300

    practicando

---
+++ main3.js
@@ -0,0 +1,26 @@
+console.log(beers)
+
+//* 2) Generar una funcion que reciba como parametro el array de cervezas y
+//* un valor de alcohol (numero). La funcion debe devolver un nuevo array con
+//* las cervezas que no excedan el nivel etílico. Cada elemento del nuevo array
+//* debe ser un objeto que tenga sólo las propiedades name, (alcohol) abv y ("amargor") ibu
+//*  para las 12:15 tiramos ruleta a ver hasta donde llegaron
+
+
+
+
+
+
+
+//* 3) Generar una función que reciba como parámetro un array de cervezas
+//* y devuelva un nuevo array con las 10 cervezas más alcohólicas
+
+//* 4) Generar una función que reciba como parámetro un array
+//* de cervezas y devuelva un nuevo array con las 10 cervezas menos amargas
+
+
+
+// 5) Generar una función que reciba como parámetro un array de cervezas
+// un nombre de propiedad y un valor booleano. Debe devolver un nuevo array
+// con 10 cervezas ordenadas por la propiedad ingresada como segundo argumento
+// de manera ascendente si el tercero es true o descendente si es false
